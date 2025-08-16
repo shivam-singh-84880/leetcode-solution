8b2bfd1700f4f2195f57564bd4f68dcd432d39ea
shivam-singh-84880 <93035716+shivam-singh-84880@users.noreply.github.com>
2025-08-16 13:33:49 +0530

    Time: 7 ms (69.12%), Space: 15.1 MB (61.37%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int find(int ind, int t, vector<int>& arr){
+    int find(int ind, int t, vector<int>& arr, vector<vector<int>>& dp){
         if(ind == 0){
             if(t==0 && arr[0]==0)return 2;
             if(t==0 || t == arr[0])return 1;
             else return 0;
         }
+        if(dp[ind][t] != -1)return dp[ind][t];
         int tk = 0;
         if(t >= arr[ind]){
-            tk = find(ind-1,t-arr[ind],arr);
+            tk = find(ind-1,t-arr[ind],arr,dp);
         }
-        int ntk = find(ind-1,t,arr);
-        return tk+ntk;
+        int ntk = find(ind-1,t,arr,dp);
+        return dp[ind][t] =  tk+ntk;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
@@ -19,12 +20,18 @@ public:
         for(auto num : nums){
             sum += num;
         }
-        
-        int t = (sum - target)/2;
-        if((sum-target)%2==1)return 0;
-        else if(sum-target < 0)return 0;
+        int t = 0;
+        if(target < 0){
+            t = (sum + target)/2;
+        }
         else{
-            return find(n-1,t,nums);
+            t = (sum - target)/2;
+        }
+        if((sum-target)%2==1)return 0;
+        else if(sum-target < 0 || sum + target < 0)return 0;
+        else{
+            vector<vector<int>> dp(n,vector<int>(t+1,-1));
+            return find(n-1,t,nums,dp);
         }
     }
 };
