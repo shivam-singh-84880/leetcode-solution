fe5697718d04e796f3f94a84762f7122057432bd
shivam-singh-84880 <93035716+shivam-singh-84880@users.noreply.github.com>
2025-08-16 20:43:33 +0530

    Time: 150 ms (36.53%), Space: 59.4 MB (11.36%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,37 +1,50 @@
 class Solution {
 public:
-    int find(int ind, int t, vector<int>& arr, vector<vector<int>>& dp){
-        if(ind == 0){
-            if(t==0 && arr[0]==0)return 2;
-            if(t==0 || t == arr[0])return 1;
-            else return 0;
+    // int find(int ind, int t, vector<int>& arr, vector<vector<int>>& dp){
+    //     if(ind == 0){
+    //         if(t==0 && arr[0]==0)return 2;
+    //         if(t==0 || t == arr[0])return 1;
+    //         else return 0;
+    //     }
+    //     if(dp[ind][t] != -1)return dp[ind][t];
+    //     int tk = 0;
+    //     if(t >= arr[ind]){
+    //         tk = find(ind-1,t-arr[ind],arr,dp);
+    //     }
+    //     int ntk = find(ind-1,t,arr,dp);
+    //     return dp[ind][t] =  tk+ntk;
+    // }
+    int find(int ind, int& t, int sum, vector<int>& nums, vector<unordered_map<int,int>>& mp){
+        if(ind < 0){
+            return sum == t ? 1 : 0;
         }
-        if(dp[ind][t] != -1)return dp[ind][t];
-        int tk = 0;
-        if(t >= arr[ind]){
-            tk = find(ind-1,t-arr[ind],arr,dp);
-        }
-        int ntk = find(ind-1,t,arr,dp);
-        return dp[ind][t] =  tk+ntk;
+        if(mp[ind].count(sum))return mp[ind][sum];
+        int pos = find(ind-1,t,sum+nums[ind],nums,mp);
+        int neg = find(ind-1,t,sum-nums[ind],nums,mp);
+        return mp[ind][sum] =  pos+neg;
     }
+
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        int sum = 0;
-        for(auto num : nums){
-            sum += num;
-        }
-        int t = 0;
-        if(target < 0){
-            t = (sum + target)/2;
-        }
-        else{
-            t = (sum - target)/2;
-        }
-        if((sum-target)%2==1)return 0;
-        else if(sum-target < 0 || sum + target < 0)return 0;
-        else{
-            vector<vector<int>> dp(n,vector<int>(t+1,-1));
-            return find(n-1,t,nums,dp);
-        }
+        vector<unordered_map<int,int>> mp(n);
+        return find(n-1,target,0,nums,mp);
+        // int n = nums.size();
+        // int sum = 0;
+        // for(auto num : nums){
+        //     sum += num;
+        // }
+        // int t = 0;
+        // if(target < 0){
+        //     t = (sum + target)/2;
+        // }
+        // else{
+        //     t = (sum - target)/2;
+        // }
+        // if((sum-target)%2==1)return 0;
+        // else if(sum-target < 0 || sum + target < 0)return 0;
+        // else{
+        //     vector<vector<int>> dp(n,vector<int>(t+1,-1));
+        //     return find(n-1,t,nums,dp);
+        // }
     }
 };
