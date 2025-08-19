5c261931d3cba39348a9b5504a66cd49139f54bf
shivam-singh-84880 <93035716+shivam-singh-84880@users.noreply.github.com>
2025-08-19 12:19:31 +0530

    Time: 3 ms (94.99%), Space: 10.3 MB (82.66%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -15,7 +15,19 @@ public:
 
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return find(n-1,amount,coins,dp);
+        vector<long> curr(amount+1,0);
+
+        for(int i=0; i<=amount; i++){
+            if(i % coins[0] == 0){
+                curr[i] = 1;
+            }
+        }
+        for(int ind=1; ind<n; ind++){
+            for(int t=coins[ind]; t<=amount; t++){
+                curr[t] += (int)curr[t-coins[ind]];
+            }
+        }
+        return curr[amount];
+        //return find(n-1,amount,coins,dp);
     }
 };
