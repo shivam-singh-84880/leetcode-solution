0577597ce1e1114d1fc4706f17e47381f6f9254f
shivam-singh-84880 <93035716+shivam-singh-84880@users.noreply.github.com>
2025-08-20 08:10:06 +0530

    Time: 8 ms (77.92%), Space: 11.4 MB (94.32%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -19,7 +19,25 @@ public:
     int minDistance(string word1, string word2) {
         int n1 = word1.size();
         int n2 = word2.size();
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
-        return find(n1,n2,word1,word2,dp);
+        vector<int> curr(n2+1,0);
+        vector<int> prev(n2+1,0);
+        for(int i=0; i<=n2; i++){
+            prev[i] = i;
+        }
+        
+        for(int ind1=1; ind1<=n1; ind1++){
+            curr[0] = ind1;
+            for(int ind2=1; ind2<=n2; ind2++){
+                if(word1[ind1-1] == word2[ind2-1]){
+                    curr[ind2] = prev[ind2-1];
+                }
+                else{
+                    curr[ind2] = 1 + min(prev[ind2],curr[ind2-1]);
+                }
+            }
+            prev = curr;
+        }
+        return prev[n2];
+        //return find(n1,n2,word1,word2,dp);
     }
 };
