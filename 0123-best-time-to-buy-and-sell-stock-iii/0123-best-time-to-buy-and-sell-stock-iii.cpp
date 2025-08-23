6c5ca2d04f852021402cee6bb50997b9fc0a8d2d
shivam-singh-84880 <93035716+shivam-singh-84880@users.noreply.github.com>
2025-08-23 10:44:55 +0530

    Time: 27 ms (78.27%), Space: 79.4 MB (85.3%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -14,23 +14,43 @@ public:
 
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> next(3,vector<int>(2,0));
-        vector<vector<int>> curr(3,vector<int>(2,0));
+        // B S B S
+        vector<int> next(5,0);
+        vector<int> curr(5,0);
 
         for(int ind=n-1; ind>=0; ind--){
-            for(int trx=1; trx<=2; trx++){
-                for(int bs=0; bs<=1; bs++){
-                    if(bs){
-                        curr[trx][bs] = max(-prices[ind] + next[trx][0], next[trx][1]);
-                    }
-                    else{
-                        curr[trx][bs] = max(prices[ind] + next[trx-1][1], next[trx][0]);
-                    }
+            for(int trx=3; trx>=0; trx--){
+                if(trx % 2 == 0){
+                    curr[trx] = max(-prices[ind] + next[trx+1], next[trx]);
                 }
+                else{
+                    curr[trx] = max(prices[ind] + next[trx+1], next[trx]);
+                }    
             }
             next = curr;
         }
-        return next[2][1];
+        return next[0];
+
+
+
+
+        // vector<vector<int>> next(3,vector<int>(2,0));
+        // vector<vector<int>> curr(3,vector<int>(2,0));
+
+        // for(int ind=n-1; ind>=0; ind--){
+        //     for(int trx=1; trx<=2; trx++){
+        //         for(int bs=0; bs<=1; bs++){
+        //             if(bs){
+        //                 curr[trx][bs] = max(-prices[ind] + next[trx][0], next[trx][1]);
+        //             }
+        //             else{
+        //                 curr[trx][bs] = max(prices[ind] + next[trx-1][1], next[trx][0]);
+        //             }
+        //         }
+        //     }
+        //     next = curr;
+        // }
+        // return next[2][1];
         //return find(0,1,2,prices,n,dp);
     }
 };
