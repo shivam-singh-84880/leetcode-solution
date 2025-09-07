773c5075c60f1dc3fa4109b98878578f92d12491
shivam-singh-84880 <93035716+shivam-singh-84880@users.noreply.github.com>
2025-09-07 12:16:42 +0530

    Time: 8 ms (95%), Space: 43.4 MB (99.17%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -56,17 +56,36 @@ public:
     //     return ans;
     // }
 
-    int sumSubarrayMins(vector<int>& arr) {
-        vector<int> nse = nsef(arr);
-        vector<int> pse = psef(arr);
-        int n = arr.size();
-        int mod = 1e9+7;
-        int sum=0;
-        for(int i=0; i<n; i++){
-            int l = i - pse[i];
-            int r = nse[i] - i;
-            sum = (sum + (l * r * 1ll * arr[i])%mod)%mod;
+    int sumSubarrayMins(vector<int>& nums) {
+        // vector<int> nse = nsef(arr);
+        // vector<int> pse = psef(arr);
+        // int n = arr.size();
+        // int mod = 1e9+7;
+        // int sum=0;
+        // for(int i=0; i<n; i++){
+        //     int l = i - pse[i];
+        //     int r = nse[i] - i;
+        //     sum = (sum + (l * r * 1ll * arr[i])%mod)%mod;
+        // }
+        // return sum;
+
+        stack<int> st;
+        long long ans=0;
+        int n = nums.size();
+        int mod=1e9+7;
+        for(int i=0; i<=n; i++){
+            int ele = i==n ? INT_MIN:nums[i];
+            while(!st.empty() && nums[st.top()] >= ele){
+                int mid = st.top();
+                st.pop();
+                int right = i;
+                int left = !st.empty() ? st.top():-1;
+                long long temp = (1ll * nums[mid]%mod * (mid-left)%mod * (right-mid)%mod)%mod;
+                ans = (ans+temp)%mod;
+            }
+            st.push(i);
         }
-        return sum;
+
+        return ans;
     }
 };
