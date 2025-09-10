cfca380d2d2ed7664662424182f59eb90fa0bedd
shivam-singh-84880 <93035716+shivam-singh-84880@users.noreply.github.com>
2025-09-10 15:52:28 +0530

    Time: 1530 ms (33.68%), Space: 9.1 MB (98.38%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -26,7 +26,18 @@ public:
 
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n+1,-1);
-        return find(0,n,s,dp)-1;
+        vector<int> dp(n+1,0);
+        for(int i=n-1; i>=0; i--){
+            int mini = INT_MAX;
+            for(int k=i; k<n; k++){
+                if(ispal(i,k,s)){
+                    int ans = 1 + dp[k+1];
+                    mini = min(mini,ans);
+                }
+            }
+            dp[i] = mini;
+        }
+        return dp[0]-1;
+        //return find(0,n,s,dp)-1;
     }
 };
