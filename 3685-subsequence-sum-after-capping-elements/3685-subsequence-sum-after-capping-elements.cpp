720ecdb6ce5df8d469e4e16dd3d253eb6c27fb8f
shivam-singh-84880 <93035716+shivam-singh-84880@users.noreply.github.com>
2025-09-15 09:22:33 +0530

    Time: 1127 ms (6.91%), Space: 57.3 MB (60%) - LeetHub

--- 3685-subsequence-sum-after-capping-elements/3685-subsequence-sum-after-capping-elements.cpp
+++ 3685-subsequence-sum-after-capping-elements/3685-subsequence-sum-after-capping-elements.cpp
@@ -1,31 +1,59 @@
 class Solution {
 public:
-    bool ispossible(int sum, int ind, int & k, vector<int>& temp){
-        if(sum == k)return true;
-        if(ind >= temp.size())return false;
-        
-        if(ispossible(sum + temp[ind], ind+1, k, temp))return true;
-        if(ispossible(sum, ind+1, k, temp)) return true;
-        
-        return false;
-        
-    }
     vector<bool> subsequenceSumAfterCapping(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<bool> ans;
-        vector<int> temp;
+        vector<vector<bool>> dp(n+1,vector<bool>(k+1,false));
+        sort(nums.begin(),nums.end());
+        for(int i=0; i<=n; i++){
+            dp[i][0] = true;
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int t=1; t<=k; t++){
+                dp[i][t] = dp[i-1][t];
+                if(nums[i-1] <= t){
+                    dp[i][t] = dp[i][t] | dp[i-1][t-nums[i-1]];
+                }
+            }
+        }
+
+        vector<bool> ans(n,false);
         for(int x=1; x<=n; x++){
-            for(int i=0; i<n; i++){
-                temp.push_back(min(x,nums[i]));
+            int ind = lower_bound(nums.begin(),nums.end(),x)-nums.begin();
+            int nrele = n-ind;
+            for(int i=0; i<=nrele; i++){
+                int rm = k-x*i;
+                if(rm >= 0 && dp[ind][rm]){
+                    ans[x-1] = true;
+                    break;
+                }
             }
-            if(ispossible(0,0,k,temp)){
-                ans.push_back(true);
-            }
-            else{
-                ans.push_back(false);
-            }
-            temp = {};
         }
         return ans;
     }
 };
+// vector<bool> subsequenceSumAfterCapping(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         vector<bool> ans(n,false);
+//         vector<int> temp;
+
+//         for(int x=1; x<=n; x++){
+//             for(int i=0; i<n; i++){
+//                 temp.push_back(min(x,nums[i]));
+//             }
+//             vector<bool> dp(k+1,false);
+//             dp[0] = true;
+//             for(int ind=0; ind<n; ind++){
+//                 for(int sum=k; sum>=temp[ind]; sum--){
+//                     dp[sum] = dp[sum] || dp[sum-temp[ind]];
+//                 }
+//             }
+
+//             if(dp[k]){
+//                 ans[x-1] = true;
+//             }
+//             temp = {};
+//         }
+
+//         return ans;
+//     }
