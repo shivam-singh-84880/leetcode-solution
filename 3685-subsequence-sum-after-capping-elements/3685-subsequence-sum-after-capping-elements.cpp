c112ef92dd12fce0411c70c63732b0e2225b06c0
shivam-singh-84880 <93035716+shivam-singh-84880@users.noreply.github.com>
2025-09-15 09:28:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3685-subsequence-sum-after-capping-elements/3685-subsequence-sum-after-capping-elements.cpp
+++ 3685-subsequence-sum-after-capping-elements/3685-subsequence-sum-after-capping-elements.cpp
@@ -2,28 +2,23 @@ class Solution {
 public:
     vector<bool> subsequenceSumAfterCapping(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<vector<bool>> dp(n+1,vector<bool>(k+1,false));
         sort(nums.begin(),nums.end());
-        for(int i=0; i<=n; i++){
-            dp[i][0] = true;
-        }
-
-        for(int i=1; i<=n; i++){
-            for(int t=1; t<=k; t++){
-                dp[i][t] = dp[i-1][t];
-                if(nums[i-1] <= t){
-                    dp[i][t] = dp[i][t] | dp[i-1][t-nums[i-1]];
-                }
-            }
-        }
-
+        
+        vector<bool> dp(k+1,false);
+        dp[0] = true;
         vector<bool> ans(n,false);
         for(int x=1; x<=n; x++){
-            int ind = lower_bound(nums.begin(),nums.end(),x)-nums.begin();
-            int nrele = n-ind;
+            int i=0;
+            while(i<n && nums[i]<x){
+                for(int t=nums[i]; t<=k; t++){
+                    dp[t] = dp[t] | dp[t-nums[i]];
+                }
+                i++;
+            }
+            int nrele = n-i;
             for(int i=0; i<=nrele; i++){
                 int rm = k-x*i;
-                if(rm >= 0 && dp[ind][rm]){
+                if(rm >= 0 && dp[rm]){
                     ans[x-1] = true;
                     break;
                 }
