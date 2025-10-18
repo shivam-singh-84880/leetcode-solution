c418ca3a761a4e02a4a6cfe3214338a73dc94ae8
shivam-singh-84880 <93035716+shivam-singh-84880@users.noreply.github.com>
2025-10-18 09:45:31 +0530

    Time: 220 ms (68.64%), Space: 173.5 MB (95.52%) - LeetHub

--- 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.cpp
+++ 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.cpp
@@ -1,43 +1,20 @@
 class Solution {
 public:
     int maxIncreasingSubarrays(vector<int>& nums) {
-        int cnt=1;
-        int l1=0;
-        int l2=0;
-        int ans=1;
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i] > nums[i-1]){
-                cnt++;
-                ans = max(ans,cnt/2);
+        int n = nums.size();
+        int ccnt = 1;
+        int pcnt = 0;
+        int mlen = 0;
+        for(int i=1; i<n; i++){
+            if(nums[i-1] < nums[i]){
+                ccnt++;
             }
             else{
-                if(l1==0){
-                    l1 = cnt;
-                    cnt = 1;
-                }
-                else if(l2==0){
-                    l2=cnt;
-                    cnt=1;
-                }
-                else{
-                    ans = max(ans,min(l1,l2));
-                    l1 = l2;
-                    l2 = cnt;
-                    cnt = 1;
-                }
+                pcnt = ccnt;
+                ccnt = 1;
             }
+            mlen = max(mlen,max(ccnt/2,min(pcnt,ccnt)));
         }
-        if(l2==0){
-            l2 = cnt;
-        }
-        else if(cnt >= l2){
-            l1 = l2;
-            l2 = cnt;
-        }
-        else if(cnt > l1){
-            l1 = cnt;
-        }
-        
-        return max(ans,min(l1,l2));
+        return mlen;
     }
 };
