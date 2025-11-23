51571648db2af82a6afc5a5f711605f5c7c1f167
shivam-singh-84880 <93035716+shivam-singh-84880@users.noreply.github.com>
2025-11-23 15:29:30 +0530

    Time: 3 ms (86.49%), Space: 28.8 MB (84.9%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,23 +1,54 @@
 class Solution {
 public:
-    int find(int ind, int sum, vector<int>& nums, vector<vector<int>>& dp){
-        if(ind < 0){
-            if(sum%3 == 0) return sum;
-            else return 0;
+    int find(int ind, int mod, vector<int>& nums, vector<vector<int>>& dp){
+        if(ind == 0){
+            if(mod == 0) return 0;
+            else return INT_MIN;
         }
 
-        if(dp[ind][sum] != -1)return dp[ind][sum];
+        if(dp[ind][mod] != -1)return dp[ind][mod];
 
-        int ntk = find(ind-1, sum, nums,dp);
-        int tk = find(ind-1, sum+nums[ind], nums,dp);
+        int ntk = find(ind-1, mod, nums,dp);
+        int tk = nums[ind-1] + find(ind-1, (mod+nums[ind-1]%3)%3, nums,dp);
 
-        return dp[ind][sum] = max(tk,ntk);
+        return dp[ind][mod] = max(tk,ntk);
     }
 
     int maxSumDivThree(vector<int>& nums) {
         int n = nums.size();
-        int ts = accumulate(nums.begin(),nums.end(),0);
-        vector<vector<int>> dp(n,vector<int>(ts+1,-1));
-        return find(n-1,0,nums,dp);
+        vector<int> prev(3,INT_MIN);
+        vector<int> curr(3,INT_MIN);
+        //return find(n,0,nums,dp);
+        prev[0] = 0;
+        for(int ind=1; ind<=n; ind++){
+            int r = nums[ind-1]%3;
+            for(int mod=0; mod<3; mod++){
+                int ntk = prev[mod];
+                int tk = nums[ind-1] + prev[(mod+r)%3];
+                curr[mod] = max(tk,ntk);
+            }
+            prev = curr;
+        }
+
+        return curr[0];
+
+        // int smone = 1e5+7;
+        // int smtwo = 1e5+7;
+        // int tot = 0;
+        // for(auto& num : nums){
+        //     tot += num;
+        //     if(num%3 == 1){
+        //         smtwo = min(smtwo, smone + num);
+        //         smone = min(smone, num);
+        //     }
+        //     if(num%3 == 2){
+        //         smone = min(smone, smtwo + num);
+        //         smtwo = min(smtwo, num);
+        //     }
+        // }
+
+        // if(tot%3 == 0)return tot;
+        // else if(tot%3 == 1) return tot - smone;
+        // else return tot - smtwo;
     }
 };
