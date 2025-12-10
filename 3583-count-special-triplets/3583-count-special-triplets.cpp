b1e287018624f8e6b2b4142480471bf9c17b1f81
shivam-singh-84880 <93035716+shivam-singh-84880@users.noreply.github.com>
2025-12-10 13:12:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.cpp
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -2,16 +2,26 @@ class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
         unordered_map<int,int> l,r;
-        int ans=0;
-        int mod=1e9+7;
+        int ans = 0;
+        int mod = 1e9+7;
         for(auto& num : nums){
-            r[num]++;
-        }
-        for(auto& num : nums){
-            r[num]--;
-            ans = (ans + (1ll * r[num*2] * l[num*2])%mod)%mod;
+            ans = (ans + r[num/2])%mod;
+            r[num] = (r[num] + l[num*2])%mod;
             l[num]++;
         }
         return ans;
+
+        // unordered_map<int,int> l,r;
+        // int ans=0;
+        // int mod=1e9+7;
+        // for(auto& num : nums){
+        //     r[num]++;
+        // }
+        // for(auto& num : nums){
+        //     r[num]--;
+        //     ans = (ans + (1ll * r[num*2] * l[num*2])%mod)%mod;
+        //     l[num]++;
+        // }
+        // return ans;
     }
 };
