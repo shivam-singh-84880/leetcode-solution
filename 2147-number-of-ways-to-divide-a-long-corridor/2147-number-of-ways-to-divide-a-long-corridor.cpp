973105633e51a97edecfe8afcf946c8c693f6fe1
shivam-singh-84880 <93035716+shivam-singh-84880@users.noreply.github.com>
2025-12-14 16:10:39 +0530

    Time: 26 ms (59.5%), Space: 33.6 MB (81.5%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -22,32 +22,45 @@ public:
     int numberOfWays(string c) {
         int n = c.size();
         int mod = 1e9+7;
-        //vector<vector<int>> dp(n+1,vector<int>(3,0));
-        vector<int> curr(3,0);
-        vector<int> next(3,0);
-        next[2] = 1;
-        for(int ind=n-1; ind>=0; ind--){
-            for(int cnt=2; cnt>=0; cnt--){
-                long long ans=0;
-                if(c[ind] == 'S'){
-                    ans += (cnt + 1 <= 2) ? next[cnt+1] : 0;
-                    if(cnt == 2){
-                        ans += next[1];
-                    }
+        int j=0;
+        int k=0;
+        int ans=1;
+        for(int i=0; i<n; i++){
+            if(c[i] == 'S'){
+                k++;
+                if(k>2 && k%2 == 1){
+                    ans = (1ll * ans * (i-j))%mod;
                 }
-                else{
-                    ans += next[cnt];
-                    if(cnt == 2){
-                        ans += next[0];
-                    }
-                }
-                ans %= mod;
-                curr[cnt] = ans;
+                j=i;
             }
-            next = curr;
         }
+        return k>0 && k%2==0 ? ans : 0;
+        //vector<vector<int>> dp(n+1,vector<int>(3,0));
+        // vector<int> curr(3,0);
+        // vector<int> next(3,0);
+        // next[2] = 1;
+        // for(int ind=n-1; ind>=0; ind--){
+        //     for(int cnt=2; cnt>=0; cnt--){
+        //         long long ans=0;
+        //         if(c[ind] == 'S'){
+        //             ans += (cnt + 1 <= 2) ? next[cnt+1] : 0;
+        //             if(cnt == 2){
+        //                 ans += next[1];
+        //             }
+        //         }
+        //         else{
+        //             ans += next[cnt];
+        //             if(cnt == 2){
+        //                 ans += next[0];
+        //             }
+        //         }
+        //         ans %= mod;
+        //         curr[cnt] = ans;
+        //     }
+        //     next = curr;
+        // }
         //return find(c,0,0,n,dp);
 
-        return next[0];
+        //return next[0];
     }
 };
