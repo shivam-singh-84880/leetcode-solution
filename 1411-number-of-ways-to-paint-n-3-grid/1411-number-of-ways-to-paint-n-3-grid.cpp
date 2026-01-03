ccfb68380c49e8173b2c940d51de358388de6e28
shivam-singh-84880 <93035716+shivam-singh-84880@users.noreply.github.com>
2026-01-03 16:05:56 +0530

    Time: 370 ms (12.67%), Space: 18.2 MB (20.67%) - LeetHub

--- 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
@@ -1,15 +1,47 @@
 class Solution {
 public:
-    int numOfWays(int n) {
-        int mod = 1e9+7;
-        long a3 = 6;
-        long b2 = 6;
-        for(int i=1; i<n; i++){
-            long na3 = (3*a3 + 2*b2)%mod;
-            long nb2 = (2*a3 + 2*b2)%mod;
-            a3 = na3;
-            b2 = nb2;
+    int mod = 1e9+7;
+    vector<vector<int>> dp;
+    string pre[12] = {{"ryr"},{"yry"},{"gry"},{"ryg"},{"yrg"},{"grg"},{"rgr"},{"ygr"},{"gyr"},{"rgy"},{"ygy"},{"gyg"}};
+    int find(int n, int prev){
+        if(n == 0)return 1;
+        if(dp[n][prev] != -1) return dp[n][prev];
+        string past = pre[prev];
+        long ans=0;
+        for(int i=0; i<12; i++){
+            if(i == prev) continue;
+            string curr = pre[i];
+            bool flag = false;
+            for(int j=0; j<3; j++){
+                if(past[j] == curr[j]){
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag){
+                ans = (ans + find(n-1,i))%mod;
+            }
         }
-        return (a3+b2)%mod;
+        return dp[n][prev] = ans;
+    }
+
+    int numOfWays(int n) {
+        long res=0;
+        dp.resize(n,vector<int>(12,-1));
+        for(int i=0; i<12; i++){
+            res = (res + find(n-1,i))%mod;
+        }
+        return res;
     }
 };
+
+//         int mod = 1e9+7;
+//         long a3 = 6;
+//         long b2 = 6;
+//         for(int i=1; i<n; i++){
+//             long na3 = (3*a3 + 2*b2)%mod;
+//             long nb2 = (2*a3 + 2*b2)%mod;
+//             a3 = na3;
+//             b2 = nb2;
+//         }
+//         return (a3+b2)%mod;
